ac31c00d40ba2d02ab84255f5109b61c79f73ee4
Hikma Hamza <h.hamza@alustudent.com>
2025-02-14 15:59:35 +0000

    Updated main.js

--- main.js
+++ main.js
@@ -2,18 +2,19 @@
 //The creation of an object that will be later changed to a string by forEach(item =>...) for match() module to work through them
 
 let str = {
+    "HTML tags" : ["<p>", '<div class="example">', '<img src="image.jpg" alt="description">']
     "Email addresses" : ["user@example.com", "firstname.lastname@company.co.uk"],
+    "Credit card numbers" : ["1234 5678 9012 3456", "1234-5678-9012-3456"],
     "URLs" : ["https://www.example.com", "http://subdomain.example.org/pageLinks"],
     "Phone Numbers" : ["(123) 456-7890", "123-456-7890", "123.456.7890"],
-    "Credit card numbers" : ["1234 5678 9012 3456", "1234-5678-9012-3456"],
-    "HTML tags" : ["<p>", '<div class="example">', '<img src="image.jpg" alt="description">']
+    
 };
 //The regular expression patterns for fetching
-let emRegex = /[a-zA-Z0-9.]+@[a-zA-Z]+\.com?(\.uk)?/g
-let urlRegex = /https?:\/\/[a-zA-Z]+\.example\.[a-zA-Z]+(\/[a-zA-Z]+)?/g
-let pnum = /\(?\d{3,}\)?(\s|\.|-)\d{3,}(-|.)\d{4,}/g
-let ccn = /\d{4,}(\s|-)\d{4,}(\s|-)\d{4,}(\s|-)\d{4,}/g
-let htmlRegex = /<[^>]+>/g
+let EmailRegex = /[a-zA-Z0-9.]+@[a-zA-Z]+\.com?(\.uk)?/g
+let linkRegex = /https?:\/\/[a-zA-Z]+\.example\.[a-zA-Z]+(\/[a-zA-Z]+)?/g
+let Phonenumber = /\(?\d{3,}\)?(\s|\.|-)\d{3,}(-|.)\d{4,}/g
+let CreditCard = /\d{4,}(\s|-)\d{4,}(\s|-)\d{4,}(\s|-)\d{4,}/g
+let HtmltagsRegex = /<[^>]+>/g
 // loop used to start an iteration
 for (key in str) {
     console.log(`the matches for ${key} are: \n`);
@@ -21,19 +22,19 @@ for (key in str) {
 str[key].forEach(item => {
     let matched;
     if (key === "HTML tags") {
-        matched = item.match(htmlRegex);
+        matched = item.match(HtmltagsRegex);
       console.log(matched);
     } else if (key === "Email addresses") {
-        matched = item.match(emRegex);
+        matched = item.match(EmailRegex);
         console.log(matched);
     } else if (key === "URLs") {
-        matched = item.match(urlRegex);
+        matched = item.match(linkRegex);
         console.log(matched);
     } else if (key === "Phone Numbers") {
-        matched = item.match(pnum);
+        matched = item.match(Phonenumber);
         console.log(matched);
     } else{
-        matched = item.match(ccn);
+        matched = item.match(CreditCard);
         console.log(matched);
     }
 })
